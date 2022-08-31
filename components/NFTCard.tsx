7ada588abf990f611b2eb69c2476262a7ad1fa68
Alex Cheung <alexc.sfu@gmail.com>
2022-08-31 01:29:55 -0700

    updated Home.tsx and NFTCard.tsx

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -1,9 +1,46 @@
-import { View, Text } from "react-native";
 import React, { FC } from "react";
+import { View, Text, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { COLORS, SIZES, SHADOWS, assets } from "../constants";
 
-const NFTCard: FC = () => {
+interface IBids {
+  id: string;
+  name: string;
+  price: number;
+  image: any;
+  date: string;
+}
+
+interface IEachCard {
+  id: string;
+  name: string;
+  creator: string;
+  price: number;
+  description: string;
+  image: any;
+  bids: IBids[];
+}
+
+interface INFTCard {
+  data: IEachCard;
+}
+
+const NFTCard: FC<INFTCard> = ({ data }: INFTCard) => {
+  const navigation = useNavigation();
+
   return (
-    <View>
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View style={{ width: "100%", height: 250 }}>
+        <Image source={data.image} />
+      </View>
       <Text>NFTCard</Text>
     </View>
   );
