857e5163f20fef17c729112d03a47a3aa033d7ab
Alex Cheung <alexc.sfu@gmail.com>
2022-08-31 00:09:56 -0700

    font set up finished

--- App.tsx
+++ App.tsx
@@ -16,7 +16,15 @@ const theme = {
 };
 
 const App: FC = () => {
-  const [loaded] = useFonts({});
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if (!loaded) return null;
 
   return (
     <NavigationContainer theme={theme}>
